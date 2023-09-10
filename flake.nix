79d312eeb2391b4d41d8c6c3b6c3bf952dff4b63
Haf <haf@protonmail.ch>
2023-09-10 19:21:15 +0200

    initial commit

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Hypermedia-based maze game";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+  in
+  {
+    devShell.${system} = pkgs.mkShell {
+      buildInputs = with pkgs; [
+          go
+      ];
+    };
+  };
+}
