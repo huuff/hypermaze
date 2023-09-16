a02db406e2a2ef481e6d5737e43ca9d08a6cec0f
Haf <haf@protonmail.ch>
2023-09-16 16:49:09 +0200

    some DRY

--- pkg/maze/ascii-view.go
+++ pkg/maze/ascii-view.go
@@ -7,6 +7,26 @@ import (
 	"github.com/samber/lo"
 )
 
+func (m Maze) AsciiView() {
+  for y := range lo.Range((m.Grid.Height * 2) + 1) {
+    for x := range lo.Range((m.Grid.Width * 2) + 1) {
+      p := grid.Point { x, y }
+      if isExteriorPoint(m.Grid, p) {
+        fmt.Print("#")
+      } else if isConnectionPoint(p) {
+        if m.isOpen(p) {
+          fmt.Print(" ")
+        } else {
+          fmt.Print("%")
+        }
+      } else {
+        fmt.Print(" ")
+      }
+    }
+    fmt.Print("\n")
+  }
+}
+
 // These are the outermost points created only for showing exterior walls and displaying the exit, they can't have any other connections
 func isExteriorPoint(g grid.Grid, p grid.Point) bool {
   return p.X == 0  || p.Y == 0 || p.X == g.Width*2 || p.Y == g.Height*2
@@ -28,56 +48,30 @@ func (m Maze) isOpen(p grid.Point) bool {
   if p.X%2 == 0 && p.Y%2 == 0 {
     // Always just a wall
     return false
-    // TODO: Please DRY
-  } else if p.X % 2 == 0 {
-    for _, direction := range []Direction { East, West } {
-      pointInDirection := direction.From(p)
-      pointInDirection = grid.Point { 
-        X: (pointInDirection.X-1)/2,
-        Y: (pointInDirection.Y-1)/2,
-      }
-
-      roomInDirection, exists := m.Rooms[pointInDirection]
-      if exists && roomInDirection.IsOpenTowards(direction.inverse()) {
-        return true
-      }
-    }
-  } else if p.Y % 2 == 0 {
-    for _, direction := range []Direction { North, South } {
-      pointInDirection := direction.From(p)
-      pointInDirection = grid.Point { 
-        X: (pointInDirection.X-1)/2,
-        Y: (pointInDirection.Y-1)/2,
-      }
-
-      roomInDirection, exists := m.Rooms[pointInDirection]
-      if exists && roomInDirection.IsOpenTowards(direction.inverse()) {
-        return true
-      }
-    }
-
+  } else if p.X % 2 == 0 && m.isOpenInDirections(p, []Direction { West, East }){
+    return true
+  } else if p.Y % 2 == 0  && m.isOpenInDirections(p, []Direction { North, South }){
+    return true
   }
 
 
   return false
 }
 
-func (m Maze) AsciiView() {
-  for y := range lo.Range((m.Grid.Height * 2) + 1) {
-    for x := range lo.Range((m.Grid.Width * 2) + 1) {
-      p := grid.Point { x, y }
-      if isExteriorPoint(m.Grid, p) {
-        fmt.Print("#")
-      } else if isConnectionPoint(p) {
-        if m.isOpen(p) {
-          fmt.Print(" ")
-        } else {
-          fmt.Print("%")
-        }
-      } else {
-        fmt.Print(" ")
-      }
+func (m Maze) isOpenInDirections(p grid.Point, directions []Direction) bool {
+  for _, direction := range directions {
+    pointInDirection := direction.From(p)
+    pointInDirection = grid.Point { 
+      X: (pointInDirection.X-1)/2,
+      Y: (pointInDirection.Y-1)/2,
     }
-    fmt.Print("\n")
+
+    roomInDirection, exists := m.Rooms[pointInDirection]
+    if exists && roomInDirection.IsOpenTowards(direction.inverse()) {
+      return true
+    }
   }
+
+  return false
 }
+
