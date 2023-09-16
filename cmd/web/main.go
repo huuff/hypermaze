527cd1f2023a88d04b7981ef47b74030f6521bd1
Haf <haf@protonmail.ch>
2023-09-16 17:43:11 +0200

    silly display

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,12 +2,35 @@ package main
 
 import (
 	"fmt"
+
+  "net/http"
+  "html/template"
+  "xyz.haff/maze/pkg/maze"
 )
+
+func index(w http.ResponseWriter, r *http.Request) {
+  ts, _ := template.ParseFiles("./cmd/web/templates/index.html.gotmpl")
+
+  templateData := struct { Mazes []*maze.Maze }{ Mazes: Mazes }
+  ts.Execute(w, templateData)
+}
 
 func main() {
   for level, maze := range Mazes {
     fmt.Println()
     fmt.Printf("Level %d:\n", level + 1)
     fmt.Println(maze.AsciiView())
+  }
+
+  mux := http.NewServeMux()
+  mux.HandleFunc("/", index)
+
+  server := http.Server {
+    Addr: ":8080",
+    Handler: mux,
+  }
+
+  if err := server.ListenAndServe(); err != nil {
+    panic(err)
   }
 }
