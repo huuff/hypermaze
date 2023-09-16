8c4b78dba5066cba0649ad888167a3282998933f
Haf <haf@protonmail.ch>
2023-09-16 17:01:03 +0200

    printing the maze

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,16 +1,20 @@
 package main
 
 import (
+	"fmt"
+
 	"xyz.haff/maze/pkg/generators"
 	"xyz.haff/maze/pkg/grid"
-  "xyz.haff/maze/pkg/maze"
+	"xyz.haff/maze/pkg/maze"
 )
 
 func main() {
-  grid := grid.Grid { Width: 5, Height: 5 }
+  grid := grid.Grid { Width: 10, Height: 10 }
   dfs := generators.NewDfsPassageGenerator(grid)
   passages := dfs.GeneratePassages()
   maze := maze.NewMaze(grid, passages)
-  maze.AsciiView()
+  asciiView := maze.AsciiView()
+
+  fmt.Println(asciiView)
 } 
 
