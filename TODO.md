485e5f5bf130bf716a22e3a0f8c46e91ac82414e
Haf <haf@protonmail.ch>
2023-09-19 20:19:04 +0200

    barely working hypermedia

--- TODO.md
+++ TODO.md
@@ -1,1 +1,2 @@
 # Tasks
+* Rename "minimap" to "ascii" everywhere
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -38,10 +38,41 @@ func (app application) minimap(w http.ResponseWriter, r *http.Request) {
   }
 }
 
+func (app application) maze(w http.ResponseWriter, r *http.Request) {
+  vars := mux.Vars(r)
+  level, err := strconv.Atoi(vars["level"])
+
+  if err != nil {
+    // TODO: Actual log for the error
+    fmt.Println(err.Error())
+    return
+  }
+
+  if level >= len(app.mazes) {
+    w.WriteHeader(http.StatusNotFound)
+    w.Write([]byte(http.StatusText(http.StatusNotFound)))
+    return
+  }
+
+  maze := app.mazes[level]
+
+  err = app.templates.partials.ExecuteTemplate(w, "maze-summary.html.gotmpl", map[string]any {
+      "Level": level,
+      "Minimap": ascii.View(*maze),
+      "Maze": maze,
+  })
+
+  if err != nil {
+    // TODO: Actual log for the error
+    fmt.Println(err.Error())
+  }
+}
+
 func (app application) routes() http.Handler {
   router := mux.NewRouter()
   router.HandleFunc("/", app.index)
   router.HandleFunc("/mazes/{level}/minimap", app.minimap)
+  router.HandleFunc("/mazes/{level}", app.maze)
   router.PathPrefix("/static").Handler(http.StripPrefix("/static", http.FileServer(http.Dir("./static/"))))
   
   return router
